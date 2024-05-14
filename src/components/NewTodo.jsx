64d45a337e4738bc7ae90da57117e86a9bf0c4e6
alserembani94 <alserembani94@gmail.com>
2024-05-14 22:34:14 +0800

    feat: add validation on title

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -1,14 +1,8 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const NewTodo = (props) => {
-  /**
-   * ✅ 1. Buat form
-   * ✅ 2. ada 2 input - title, description
-   * ✅ 3. ada 1 button - submit
-   * 4. ketika button di klik, maka akan memanggil function onSubmit
-   * 5. extract input daripada form
-   * 6. panggil api
-   */
+  const [title, setTitle] = useState('');
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -33,21 +27,23 @@ const NewTodo = (props) => {
       padding: '1rem',
       backgroundColor: '#444444',
       borderRadius: '0.5rem',
-    }}>
+    }} disabled={!title.trim()}>
       <fieldset style={{
         display: 'flex',
         flexDirection: 'column',
         gap: '0.5rem',
       }}>
         <label htmlFor="title">Title</label>
-        <input type="text" id="title" name="title" style={{
+        <input type="text" id="title" name="title" onChange={(event) => {
+          setTitle(event.target.value);
+        }} style={{
           padding: '0.5rem 0.75rem',
           borderRadius: '0.25rem',
           border: 'none',
           backgroundColor: '#ffffff',
           color: '#000000',
           outline: 'none',
-        }} />
+        }} required />
       </fieldset>
 
       <fieldset style={{
@@ -66,7 +62,7 @@ const NewTodo = (props) => {
         }} />
       </fieldset>
 
-      <button type="submit">Submit</button>
+      <button type="submit" className="submit" disabled={!title.trim()}>Submit</button>
     </form>
   )
 }

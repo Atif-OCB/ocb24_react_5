3202db76eb0a88d845cdec14f895e37e4c704618
alserembani94 <alserembani94@gmail.com>
2024-05-14 22:57:54 +0800

    feat: update status for todo list

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import Card from "./components/Card";
 import NewTodo from "./components/NewTodo";
 
+const generateRandomHex = () => {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
+
 function App() {
   const [todos, setTodos] = useState([]);
 
@@ -10,7 +14,7 @@ function App() {
     description,
   }) => {
     const newTodoItem = {
-      id: Math.random().toString(),
+      id: generateRandomHex(),
       title,
       description,
       checked: false,
@@ -19,11 +23,42 @@ function App() {
     setTodos((prevTodos) => {
       return [...prevTodos, newTodoItem];
     });
-
   }
 
+  const handleChecked = (event) => {
+    const targetId = event.target.id;
+    const targetChecked = event.target.checked;
+
+    setTodos((prevTodo) => {
+      const updatedTodos = prevTodo.map((todo) => {
+        // Return early if id not match - because we don't want to change anything
+        if (todo.id !== targetId) {
+          return todo;
+        }
+
+        // Now, since id is matched, we change the `checked`
+        const updatedItem = {
+          // Copy existing todo item
+          ...todo,
+          // Update checked
+          checked: targetChecked,
+        }
+
+        // return updated todo item
+        return updatedItem;
+      })
+
+      return updatedTodos;
+    })
+  };
+
   return (
-    <>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1rem',
+      padding: '2rem',
+    }}>
       <NewTodo addNewTodo={handleAddNewTodo} />
       {
         todos.map((todo) => {
@@ -34,14 +69,12 @@ function App() {
               title={todo.title}
               description={todo.description}
               checked={todo.checked}
-              toggleDone={(event) => {
-                // setTest(event.target.checked);
-              }}
+              toggleDone={handleChecked}
             />
           );
         })
       }
-    </>
+    </div>
   )
 }
 
